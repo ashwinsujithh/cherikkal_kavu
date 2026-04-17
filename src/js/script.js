49ca65474cde5f9f982d67f49450955d9079fe2c
ashwin sujith <ashwinsujith556@gmail.com>
2026-04-17 20:56:19 +0530

    created popup for gods

--- src/js/script.js
+++ src/js/script.js
@@ -26,3 +26,72 @@ const nav = document.getElementById("navMenu");
 toggle.addEventListener("click", () => {
 	nav.classList.toggle("active");
 });
+
+const modal = document.getElementById("theyyamModal");
+const modalTitle = document.getElementById("modalTitle");
+const modalBody = document.getElementById("modalBody");
+const closeBtn = document.querySelector(".close-btn");
+
+// open modal
+document.querySelectorAll(".gallery-item").forEach(item => {
+	item.addEventListener("click", () => {
+
+		// ignore items without data
+		if (!item.dataset.title) return;
+
+		modalTitle.innerText = item.dataset.title;
+		modalBody.innerText = item.dataset.content;
+
+		modal.classList.add("active");
+		document.body.style.overflow = "hidden"; // lock scroll
+	});
+});
+
+// close button
+closeBtn.addEventListener("click", closeModal);
+
+// outside click
+modal.addEventListener("click", (e) => {
+	if (e.target === modal) closeModal();
+});
+
+// ESC key
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") closeModal();
+});
+
+function closeModal() {
+	modal.classList.remove("active");
+	document.body.style.overflow = "auto";
+}
+
+let currentLang = "ml";
+let currentItem = null;
+
+document.querySelectorAll(".gallery-item").forEach(item => {
+	item.addEventListener("click", () => {
+		if (!item.dataset.title) return;
+
+		currentItem = item;
+		currentLang = "ml";
+
+		modalTitle.innerText = item.dataset.title;
+		modalBody.innerText = item.dataset.contentMl;
+
+		modal.classList.add("active");
+		document.body.style.overflow = "hidden";
+	});
+});
+
+document.getElementById("translateBtn").addEventListener("click", () => {
+	if (!currentItem) return;
+
+	if (currentLang === "ml") {
+		modalBody.innerText = currentItem.dataset.contentEn;
+		currentLang = "en";
+	} else {
+		modalBody.innerText = currentItem.dataset.contentMl;
+		currentLang = "ml";
+	}
+	translateBtn.innerText = currentLang === "ml" ? "EN" : "ML";
+});
